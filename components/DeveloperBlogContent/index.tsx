2413f65aea793719a2d48e33cda91a43088add4a
steve-kim-96 <soungku@hotmail.com>
2021-03-06 18:33:25 +1300

    able to bring up content that matches param id

--- components/DeveloperBlogContent/index.tsx
+++ components/DeveloperBlogContent/index.tsx
@@ -1,21 +1,28 @@
 import PageListItem from '../../lib/interfaces/PageListItem'
-import ImageList from '../ImageList'
 import React from 'react'
+import { useRouter } from 'next/router'
 
 export interface Props {
   items?: PageListItem[],
 }
 
-const DeveloperBlogItems = ({ items = [] }: Props) => (
-  <ul>
-    {items.map(item =>
-      <li key={item.id}>
-          <p>{item.date}</p>
-          <h1><strong>{item.title}</strong></h1>
-          <p>{item.body}</p>
-      </li>
-    )}
-  </ul>
-)
+const DeveloperBlogItems = ({ items = [] }: Props) => {
+  const router = useRouter()
+  const { id } = router.query
+  return (
+    <ul>
+      {items.map(item => {
+        return item.id === Number(id)
+          ? <li key={item.id}>
+            <p>{item.date}</p>
+            <h1><strong>{item.title}</strong></h1>
+            <p>{item.body}</p>
+          </li>
+          : null
+      }
+      )}
+    </ul>
+  )
+}
 
 export default DeveloperBlogItems
