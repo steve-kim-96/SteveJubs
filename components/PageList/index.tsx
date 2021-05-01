889e9c821db4e2387e482202a437d6197fc8021f
steve-kim-96 <soungku@hotmail.com>
2021-05-01 12:12:40 +1200

    re-aligning style for sass

--- components/PageList/index.tsx
+++ components/PageList/index.tsx
@@ -1,5 +1,4 @@
 import PageListItem from '../../lib/interfaces/PageListItem'
-import ImageList from '../ImageList'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
@@ -11,19 +10,16 @@ export interface Props {
 const PageList = ({ items = [] }: Props) => {
   const { pathname } = useRouter()
   return (
-    <div className="w-full grid xl:grid-cols-4 lg:grid-cols-3 gap-4 gap-x-4 rounded-md lg:px-6 xl:px-10 lg:mt-14">
-    <ul>
-      {items.map(item =>
+    <div className="blog-list">
+      <ul>
+        {items.map(item =>
           <li key={item.id}>
             <Link href={`${pathname}/${item.id}`}>
-              <a>
-                <h1><strong>{item.title}</strong></h1>
-                <ImageList width={1024} height={576} {...item.img} />
-              </a>
+              <h1><a><strong>{item.title}</strong></a></h1>
             </Link>
           </li>
-      )}
-    </ul>
+        )}
+      </ul>
     </div>
   )
 }
