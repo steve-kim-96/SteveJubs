5693499f148c0ecaabd8fa4dcf92f59c41ef9e4d
steve-kim-96 <soungku@hotmail.com>
2021-05-05 11:33:21 +1200

    Add in image for each blog post

--- components/BlogContent/index.tsx
+++ components/BlogContent/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import PageListItem from '../../lib/interfaces/PageListItem'
 import React from 'react'
 import { useRouter } from 'next/router'
@@ -9,18 +10,21 @@ export interface Props {
 const BlogItems = ({ items = [] }: Props) => {
   const { id } = useRouter().query
   return (
-    <ul>
-      {items.map(item => {
-        return item.id === Number(id)
-          ? <li key={item.id}>
-            <p>{item.date}</p>
-            <h1><strong>{item.title}</strong></h1>
-            {item.body}
-          </li>
-          : null
-      }
-      )}
-    </ul>
+    <div className='blog-content'>
+      <ul>
+        {items.map(item => {
+          return item.id === Number(id)
+            ? <li key={item.id}>
+              <p>{item.date}</p>
+              <h1><strong>{item.title}</strong></h1>
+              {item.body}
+              <Image alt={item.img.alt} src={item.img.src} width={570} height={300}/>
+            </li>
+            : null
+        }
+        )}
+      </ul>
+    </div>
   )
 }
 
